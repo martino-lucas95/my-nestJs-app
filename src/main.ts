4809a107bdada7c356f58d482881fecd09130a66
Lucas Martino <71530057+martino-lucas95@users.noreply.github.com>
2025-10-17 15:56:43 -0300

    feat: enable Swagger documentation in development mode
    - Configure Swagger setup to be available only in development environment
    - Log messages indicating Swagger availability or production mode status

--- src/main.ts
+++ src/main.ts
@@ -7,22 +7,26 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  // 📘 Configuración Swagger
-  const config = new DocumentBuilder()
-    .setTitle('MyApp API')
-    .setDescription('Documentación de la API Gateway')
-    .setVersion('1.0')
-    .addTag('clients')
-    .addTag('invoices')
-    .build();
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('My App API')
+      .setDescription('Documentación de la API Gateway')
+      .setVersion('1.0')
+      .addTag('clients')
+      .addTag('invoices')
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    console.log('📘 Swagger disponible en http://localhost:3000/api');
+  } else {
+    console.log('🚫 Swagger deshabilitado en modo producción');
+  }
   await app.listen(process.env.PORT ?? 3000);
 
   console.log(
     `🚀 Gateway corriendo en http://localhost:${process.env.PORT ?? 3000}`,
   );
-  console.log('📘 Swagger disponible en http://localhost:3000/api');
 }
 bootstrap();
